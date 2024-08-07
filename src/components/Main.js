22e1fe24254bbec4d4f529fdf84b4f945e43c4c4
Cujo26 <contact@cujo26.xyz>
2024-08-07 12:40:59 +0200

    removed some height, positioned discord widget

--- src/components/Main.js
+++ src/components/Main.js
@@ -14,7 +14,9 @@ export default function Main() {
 						</a>
 						!
 					</p>
-					<iframe src="https://discord.com/widget?id=783315747283795969&theme=dark" width="350" height="500" allowtransparency="true" frameborder="0" sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts" />
+					<div className="flex flex-col w-full mb-2 text-center items-center py-4">
+						<iframe src="https://discord.com/widget?id=783315747283795969&theme=dark" width="350" height="300" allowtransparency="true" frameborder="0" sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts" />
+					</div>
 				</div>
 			</div>
 
