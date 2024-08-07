65515276e8970da338b8d5df9877aded15ddb86b
Cujo26 <contact@cujo26.xyz>
2024-08-07 12:35:34 +0200

    added iframe

--- src/components/Main.js
+++ src/components/Main.js
@@ -14,6 +14,7 @@ export default function Main() {
 						</a>
 						!
 					</p>
+					<iframe src="https://discord.com/widget?id=783315747283795969&theme=dark" width="350" height="500" allowtransparency="true" frameborder="0" sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts" />
 				</div>
 			</div>
 
